673006cfb657387dabcc4a0391adaf69232e162d
Mile Mitrovic <milemitrovic888@gmail.com>
2023-10-08 20:36:08 +0200

    first commit

--- source/predict.py
+++ source/predict.py
@@ -18,26 +18,40 @@ class PredictConversions:
                  model: str=None,
                  index_row: int=1, 
                  ) -> None:
+        """
+        Predict raw data - pipeline.
         
+        Args:
+             data: root of the raw data (in this case root of the ds_homework_sample.csv).
+             model: chose the model {LR, KNN, RF, XGB}.
+             index_row: chose the sample from the raw dataset.
+        """
         self.data = data
         self.model = model
         self.index_row = index_row
 
     def predict(self):
+        """ Predict the probability of the conversion using input features"""
+        
+        # Upload data
         df_predict = pd.read_csv(self.data, sep='|')
-
+        
+        # Define Label Encoder for decoding specific features
         encoder = LabelEncoder()
 
-        # Encode the string data into a new column
+        # Specific features
         string_data = ['geo', 'os_type', 'os', 'os_version', 'device_type', 'device',
                        'browser', 'lang', 'proxy', 'netspeed']
-
+         
+        # Encode the string data into a new columnS
         for i in string_data:
             df_predict[i] = encoder.fit_transform(df_predict[i])
         
+        # Load k best features for which the model was trained 
         k_best_features = pickle.load(open('save_models/best_features.pickle', 'rb'))
         X_pred = df_predict.loc[int(self.index_row), k_best_features]
-
+        
+        # Chose and load trained model
         if self.model == 'LR':
             model = pickle.load(open('save_models/lr_model.pickle', 'rb'))
         elif self.model == 'KNN':
@@ -47,7 +61,7 @@ class PredictConversions:
         elif self.model == 'XGB':
             model = pickle.load(open('save_models/xgb_model.pickle', 'rb'))
     
-
+        # Predict probability of the conversion for the input data
         y_pred_prob = model.predict_proba(np.array(X_pred).reshape(1, -1))
 
         return y_pred_prob[:,1]
--- source/train_validate.py
+++ source/train_validate.py
@@ -19,6 +19,17 @@ class TrainModel:
                  n_calls: int=30,
                  model_treshold: int=0.5
                  ) -> None:
+        """
+        Train validation pipeline.
+        
+        Args:
+             dataset_root_train: root of the train.csv.
+             dataset_root_test: root of the test.csv.
+             model: chose the model {LR, KNN, RF, XGB}.
+             k_best_features: number of the best features.
+             n_calls: number of calls for the Bayesian hyperparameters optimization.
+             model_treshold: treshold of the classifier function.
+        """
         
         self.dataset_root_train = dataset_root_train
         self.dataset_root_test = dataset_root_test
@@ -29,6 +40,8 @@ class TrainModel:
 
     @staticmethod
     def get_best_features(selected_features, score): 
+        """ Plot the order best features using mrmr approach"""
+
         feature_scores = pd.DataFrame(score, index=selected_features, columns=['score'])
         
         y_ = np.arange(0, len(selected_features))
@@ -49,27 +62,37 @@ class TrainModel:
         fig.show()
 
     def train_model(self):
+        """ Train\Validate choosen classifiers """
+
+        # Upload datasets
         df_train = pd.read_csv(self.dataset_root_train)
         df_test = pd.read_csv(self.dataset_root_test)
-
+        
+        # Create input features and labeled output for training 
         X_train = df_train.drop(['cp'], axis=1)
         y_train = df_train['cp']
-    
+        
+        # Create input features and labeled output for validation 
         X_test = df_test.drop(['cp'], axis=1)
         y_test = df_test['cp']
 
-
+        # Run mrmr approach for finding the k best features 
         selected_features, score = mrmr_selection(X_train, y_train, K = self.k_best_features, relevance = 'mi_class', redundancy = 'pearson', denominator = 'mean')
         
-        all_features, all_score = mrmr_selection(X_train, y_train, K = 22, relevance = 'mi_class', redundancy = 'pearson', denominator = 'mean')
+        # Run mrmr approach for ordrering all features by mrmr approach 
+        all_features, all_score = mrmr_selection(X_train, y_train, K = 20, relevance = 'mi_class', redundancy = 'pearson', denominator = 'mean')
+        # Run plot
         TrainModel.get_best_features(all_features, all_score)
         
+        # Save the best k features
         with open('save_models/best_features.pickle', 'wb') as file:
             pickle.dump(selected_features, file)
-
+         
+        # Select the best k features for the training and validation
         X_train_best = X_train.loc[:, selected_features]
         X_test_best = X_test.loc[:, selected_features]
-
+        
+        # Chose model, train and predict probabaility of the conversion
         if self.model == 'LR':
             model_train = LR(X_train_best, y_train, n_calls=self.n_calls, normalize=False)
             y_pred_prob = model_train.predict_proba(X_test_best)
@@ -85,7 +108,7 @@ class TrainModel:
         else:
             raise Exception('You must choose a model!')
         
-
+        # Set model treshold
         model_threshold = self.model_treshold
         y_pred_treshold = (y_pred_prob[:,1] >= model_threshold).astype(int)
         
